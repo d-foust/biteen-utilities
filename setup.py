d40e60635f1745f5a84a5925fb0978c483b7c504
Daniel Foust <djfoust@umich.edu>
2023-12-08 14:37:25 -0500

    add dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,19 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'h5py',
+        'hdf5storage',
+        'matplotlib',
+        'nd2',
+        'numpy',
+        'pandas',
+        'scipy',
+        'tifffile',
+        'napari[all]',
+        'napari_animation',
+        'scikit-image'
+    ],
     classifiers=(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
