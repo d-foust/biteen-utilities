46fdd361d9fda57e2ab1406e025e525ccafe793e
Daniel Foust <djfoust@umich.edu>
2024-11-25 17:35:03 -0500

    increment version number for bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.md", "r") as f:
 
 setuptools.setup(
     name='biteen_utilities',
-    version='0.1.1',
+    version='0.1.2',
     author='Daniel Foust',
     author_email='djfoust@umich.edu',
     description='Tools for working with single-molecule localization data.',
